c5c9a0300e221f7c739bb5d8ab89659fb8f3d2a3
Shrey Pant <pantshrey01@gmail.com>
2024-01-14 00:53:27 +0530

    added validation

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
-
+const {createTodo, updateTodo} = require('./types');
 const app = express() ;
 
 app.use(express.json());
 
 app.post('/todo',(req,res)=>{
+        const todo = req.body;
+        const parseTodo=createTodo.safeParse(todo);
+        if(!parseTodo.success)
+        {
+            res.status(411).json({msg:"Wrong inputs"});
+        }
 
 })
 
@@ -13,7 +19,15 @@ app.get('/todos',(req,res)=>{
 })
 
 app.put('/completed',(req,res)=>{
-
+    const todoID  = req.body;
+    const parsetodoID = updateTodo.safeParse(todoID);
+    if(!parsetodoID)
+    {
+        res.status(411).json({
+            msg:"Wrong inputs",
+        });
+    }
+    
 })
 
 app.listen(3000,(err)=>{
